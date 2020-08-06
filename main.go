5d5533f450f07d07229aac8f53903743c2490e94
jipeng <jp209529@alibaba-inc.com>
2020-08-06 20:41:09 +0800

    修改

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"github.com/micro/go-micro/v2"
 	log "github.com/micro/go-micro/v2/logger"
 	"rule-srv/src/handler"
-	"rule-srv/subscriber"
 
 	rulesrv "rule-srv/proto/rule-srv"
-	_ "rule-srv/src/services/mongodb"
-	_ "rule-srv/src/services/redis"
 )
 
 func main() {
@@ -23,9 +20,6 @@ func main() {
 
 	// Register Handler
 	rulesrv.RegisterRuleSrvHandler(service.Server(), new(handler.RuleSrv))
-
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("go.micro.service.rule-srv", service.Server(), new(subscriber.RuleSrv))
 
 	// Run service
 	if err := service.Run(); err != nil {
